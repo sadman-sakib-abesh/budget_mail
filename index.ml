1f70cb2f10ea46136ea41c75ccf322541a8af1f3
sadman-sakib-abesh <63991795+sadman-sakib-abesh@users.noreply.github.com>
2021-09-23 11:56:35 +0600

    Update index.ml

--- index.ml
+++ index.ml
@@ -1,1 +1,13 @@
 
+<html>
+    <head>
+        <meta name="yandex-verification" content="32612a215c69f92d" />
+        <title>budget mail</title>
+        <style>
+
+        </style>
+    </head>
+    <body>
+
+    </body>
+</html>
